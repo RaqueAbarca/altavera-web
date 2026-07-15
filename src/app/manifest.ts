0b15a18425e662e6ad0d9a7e22d6799664b33e6c
RaqueAbarca <raquelsofia.abarcaespinoza@gmail.com>
2026-07-15 14:45:13 -0600

    icono apple con fondo

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -13,18 +13,8 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#2E5E3E",
     icons: [
       {
-        src: "/icon.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        src: "/icon.png",
+        src: "/apple-icon.png",
         sizes: "512x512",
-        type: "image/png",
-      },
-      {
-        src: "/icon.png",
-        sizes: "any",
         type: "image/png",
       },
     ],
